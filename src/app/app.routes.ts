9ea04d64a122f929b371bcc938fd4d9666c6cc5d
Alejandro Martinez <mart.perez93@gmail.com>
2025-05-31 11:49:50 +0200

    feat: update routing to include wildcard redirect to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
-    
+    {path: 'desafios', loadComponent: () => import('./components/challenge-list/challenge-list.component').then(m => m.ChallengeListComponent)},
+    {path: '**' , redirectTo: 'home'}
 ];
