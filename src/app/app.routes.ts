f6c66d36ce061131f0f3daafc6ab8c998e17ed16
Alejandro Martinez <mart.perez93@gmail.com>
2025-05-31 12:15:34 +0200

    feat: add route for ChallengeComponent with dynamic ID parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
     {path: 'desafios', loadComponent: () => import('./components/challenge-list/challenge-list.component').then(m => m.ChallengeListComponent)},
+    {path: 'desafios/:id', loadComponent: () => import('./components/challenge/challenge.component').then(m => m.ChallengeComponent)},
     {path: '**' , redirectTo: 'home'}
 ];
