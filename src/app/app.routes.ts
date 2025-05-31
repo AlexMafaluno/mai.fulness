a7431cb3db588a332c011f377b96caf62a5704fd
Alejandro Martinez <mart.perez93@gmail.com>
2025-05-31 12:06:33 +0200

    feat: update wildcard route to load NotFoundComponent instead of redirecting to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
     {path: 'desafios', loadComponent: () => import('./components/challenge-list/challenge-list.component').then(m => m.ChallengeListComponent)},
-    {path: '**' , redirectTo: 'home'}
+    
+    {path: '**' , loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent)}
 ];
