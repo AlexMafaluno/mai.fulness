5956371a6a079268b6625ce10794d6882f274efd
Alejandro Martinez <mart.perez93@gmail.com>
2025-05-31 12:08:17 +0200

    fix: remove unnecessary blank line in routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,5 @@ export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
     {path: 'desafios', loadComponent: () => import('./components/challenge-list/challenge-list.component').then(m => m.ChallengeListComponent)},
-    
     {path: '**' , loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent)}
 ];
